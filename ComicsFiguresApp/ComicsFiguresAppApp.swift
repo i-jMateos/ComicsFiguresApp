73268446b0529b1f7e2ec7a5952295eeaa59f434
Jordi <jordimatman@gmail.com>
2025-07-17 00:20:12 +0200

    Initial Commit

---
+++ ComicsFiguresApp/ComicsFiguresAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComicsFiguresAppApp.swift
+//  ComicsFiguresApp
+//
+//  Created by Jordi on 17/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ComicsFiguresAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComicsFiguresAppTests/ComicsFiguresAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  ComicsFiguresAppTests.swift
+//  ComicsFiguresAppTests
+//
+//  Created by Jordi on 17/7/25.
+//
+
+import Testing
+
+struct ComicsFiguresAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
